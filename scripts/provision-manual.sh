8f72d55e6d894b25c2b90061dc777cf7aec6bf27
root <jindrich.kana@elostech.cz>
2018-03-27 17:01:26 -0400

    repo init & push

---
+++ scripts/provision-manual.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+oc new-project dev --display-name="Tasks - Dev"
+oc new-project stage --display-name="Tasks - Stage"
+oc new-project cicd --display-name="CI/CD"
+
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n dev
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n stage
+
+oc new-app -n cicd -f https://raw.githubusercontent.com/mipam007/pipeline/master/cicd-template.yaml --param DEV_PROJECT=dev --param STAGE_PROJECT=stage
+
